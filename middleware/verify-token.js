1bcc414bbd05c3911017a2197c11f532f0720b24
mohd <generalassemblyse@gmail.com>
2025-08-12 14:22:49 +0300

    payload

--- middleware/verify-token.js
+++ middleware/verify-token.js
@@ -5,7 +5,7 @@ function verifyToken(req, res, next) {
     const token = req.headers.authorization.split(' ')[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     
-    req.user = decoded.payload;
+    req.user = decoded;
     
     next();
   } catch (err) {
